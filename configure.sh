874f8b3adcac8d7530e6f06124d431322716a73a
Jeremy Echols <jechols@uoregon.edu>
2017-07-17 13:46:25 -0700

    Add configure.sh

---
+++ configure.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+#
+# Copies the various configuration files into their respective locations
+set -eu
+
+force=0
+if [[ ${1:-} == "--force" ]]; then
+  force=1
+fi
+
+copyfile() {
+  source=$1
+  destination=$2
+
+  if [[ $force == 1 ]]; then
+    rm -f $destination
+  fi
+
+  if [[ -f $destination ]]; then
+    echo "Cowardly refusing to overwrite $destination; use --force if you really want this"
+  else
+    cp $source $destination
+  fi
+}
+
+copyfile settings/onisite/urls.py oni/onisite/urls.py
+copyfile settings/onisite/settings_local.py oni/onisite/settings_local.py
+copyfile settings/featured/config.py oni/onisite/plugins/featured_content/config.py
