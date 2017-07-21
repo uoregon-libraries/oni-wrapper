9c735ac807b0b7f374f032cb474dd55f20e35bf8
Jeremy Echols <jechols@uoregon.edu>
2017-07-21 11:59:05 -0700

    Make static pages top-level
    This only happens when no other routing matches

--- settings/onisite/urls.py
+++ settings/onisite/urls.py
@@ -2,8 +2,8 @@ from django.conf.urls import url, include
 
 urlpatterns = [
     url('', include("themes.oregon.urls")),
-    url('^static/', include("onisite.plugins.staticpages.urls")),
     url(r'^map$', include("onisite.plugins.map.urls")),
     url(r'^$', include("onisite.plugins.featured_content.urls")),
     url('', include("core.urls")),
+    url('', include("onisite.plugins.staticpages.urls")),
 ]
