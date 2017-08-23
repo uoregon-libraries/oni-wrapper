bf9248bfd97c667a3d345ee5bd567a4be1c5e45c
Jeremy Echols <jechols@uoregon.edu>
2017-08-23 14:40:41 -0700

    Remove featured content's URLs

--- settings/onisite/urls.py
+++ settings/onisite/urls.py
@@ -3,7 +3,6 @@ from django.conf.urls import url, include
 urlpatterns = [
     url('', include("themes.oregon.urls")),
     url(r'^map$', include("onisite.plugins.map.urls")),
-    url(r'^$', include("onisite.plugins.featured_content.urls")),
     url('', include("onisite.plugins.calendar.urls")),
     url('', include("core.urls")),
     url('', include("onisite.plugins.staticpages.urls")),
