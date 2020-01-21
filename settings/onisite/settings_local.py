d3f6445d84d267046158badb44c7eb4a21b23764
Jeremy Echols <jechols@uoregon.edu>
2020-01-21 09:38:47 -0800

    Replace settings with latest example file

--- settings/onisite/settings_local.py
+++ settings/onisite/settings_local.py
@@ -1,66 +1,25 @@
 import os
-import logging
 import urllib
 
-###
-# Site customizations: these settings are what make oregonnews
-###
+# For initial customization, search and update values beginning with 'YOUR_'
 
-SITE_TITLE = "Historic Oregon Newspapers"
-PROJECT_NAME = "Oregon Digital Newspapers Initiative"
-STATIC_PAGES_PATH = "/opt/openoni/themes/oregon/pages"
-TIME_ZONE = 'America/Los_Angeles'
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-INSTALLED_APPS = (
-    'django.contrib.humanize',
-    'django.contrib.staticfiles',
+################################################################
+# ENVIRONMENT SETTINGS
+################################################################
+# BASE_URL can NOT include any path elements!
+BASE_URL = os.getenv('ONI_BASE_URL', 'http://localhost')
+url = urllib.parse.urlparse(BASE_URL)
+ALLOWED_HOSTS = [url.netloc]
 
-    # Make sure oregon theme files override plugin and core pages
-    'themes.oregon',
-
-    'onisite.plugins.title_locations',
-    'onisite.plugins.featured_content',
-    'onisite.plugins.map',
-    'onisite.plugins.calendar',
-    'core',
-
-    # These should come last so static pages can't accidentally override theme/core pages
-    'onisite.plugins.staticpages',
-)
-
-###
-# Site-specific settings we are keeping as-is from ONI defaults
-###
-
-LOG_LOCATION = '/opt/openoni/log/'
-CONN_MAX_AGE = 30
-
-# Absolute path on disk to the data directory
-STORAGE = os.getenv('ONI_STORAGE_PATH', '/opt/openoni/data/')
-
-# Various storage subdirectories
-BATCH_STORAGE = os.path.join(STORAGE, 'batches')
-COORD_STORAGE = os.path.join(STORAGE, 'word_coordinates')
-OCR_DUMP_STORAGE = os.path.join(STORAGE, 'ocr')
-TEMP_TEST_DATA = os.path.join(STORAGE, 'temp_test_data')
-
-# URL path to the data directory
-STORAGE_URL = '/data/'
-
-# Displays newspaper titles with medium ("volume", "microform") when available
-TITLE_DISPLAY_MEDIUM = False
-
-###
-# ONI environment-specific settings
-###
-
-# 1. Stuff that could be in openoni.ini, but which we now pull from the
-# environment to ease cloudiness
-
-BASE_URL = os.getenv("ONI_BASE_URL")
-
-RESIZE_SERVER = BASE_URL + '/images/resize'
-TILE_SERVER = BASE_URL + '/images/iiif'
+if url.scheme == 'https':
+    """
+    Enable HSTS by setting SECURE_HSTS_SECONDS > 0.
+    Test with a low value (e.g. 300)
+    before setting a high value (e.g. 15552000) for long-term use
+    """
+    SECURE_HSTS_SECONDS = int(os.getenv('ONI_HSTS_SECONDS', 0))
 
 DATABASES = {
     'default': {
@@ -74,92 +33,199 @@ DATABASES = {
     }
 }
 
+DEBUG = True if os.getenv('ONI_DEBUG', 0) == '1' else False
+
+# IIIF server public URL endpoint
+IIIF_URL = os.getenv('ONI_IIIF_URL', 'http://localhost/images/iiif')
+
+SECRET_KEY = os.getenv('ONI_SECRET_KEY', 'openoni')
+
 SOLR_BASE_URL = os.getenv('ONI_SOLR_URL', 'http://solr:8983')
-SECRET_KEY = os.getenv("ONI_SECRET_KEY")
 
-# DEBUG defaults to false, and is only true if ONI_DEBUG is set to "1" to avoid
-# any kind of accident putting production into debug mode
-DEBUG = False
-if os.getenv("ONI_DEBUG") == "1":
-    logging.getLogger(__name__).info("Enabling debug settings")
-    DEBUG = True
+# Absolute path on disk to the data directory
+STORAGE = os.getenv('ONI_STORAGE_PATH', os.path.join(BASE_DIR, 'data'))
 
-# 2. Stuff which hinges on other environmental settings
-RESIZE_SERVER = BASE_URL + "/images/resize"
-TILE_SERVER = BASE_URL + "/images/iiif"
+"""
+Django logging outputs in Apache logs by default.
+Log to file when Apache logs don't provide info or tracebacks.
+Ensure file is writeable by Apache user with SELinux writeable context:
+    chcon -t httpd_sys_rw_content_t /path/to/debug.log
+Start with INFO level; change to DEBUG if insufficient
+"""
+if os.getenv('ONI_LOG_TO_FILE', 0) == '1':
+    LOGGING = {
+        'version': 1,
+        'disable_existing_loggers': False,
+        'handlers': {
+            'file': {
+                'level': 'INFO',
+                'class': 'logging.FileHandler',
+                'filename': os.path.join(BASE_DIR, 'log', 'debug.log'),
+            },
+        },
+        'loggers': {
+            'django': {
+                'handlers': ['file'],
+                'level': 'INFO',
+                'propagate': True,
+            },
+        },
+    }
 
-IS_PRODUCTION = not DEBUG
 
-# If we're in an https environment, let's make sure we do it right
-url = urllib.parse.urlparse(BASE_URL)
-if url.scheme == "https":
-    logging.getLogger(__name__).info("Enabling HTTPS settings")
-    # HTTPS Settings
-    CSRF_COOKIE_SECURE = True
-    SESSION_COOKIE_SECURE = True
+################################################################
+# DJANGO SETTINGS
+################################################################
+# Keep database connections open until idle for this many seconds
+CONN_MAX_AGE = 30
 
-    # Enable HSTS by setting SECURE_HSTS_SECONDS > 0
-    SECURE_HSTS_INCLUDE_SUBDOMAINS = True
-    SECURE_HSTS_PRELOAD = True
+# List of configuration classes / app packages in order of priority high to low.
+# The first item in the list has final say when collisions occur.
+INSTALLED_APPS = (
+    # Default
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
 
-    # Test with a low value (e.g. 300)
-    # before setting a high value (e.g. 15552000) for long-term use
-    SECURE_HSTS_SECONDS = 300
-    SECURE_SSL_REDIRECT = True
+    # Humanize and local theme override all below
+    'django.contrib.humanize',  # Makes data more human-readable
+#    'themes.YOUR_THEME_NAME',
 
-ALLOWED_HOSTS = [url.netloc]
+    # Plugins
+    # See https://github.com/open-oni?q=plugin for available plugins
 
-###
-# Core ONI settings we want to preserve unless something major changes - don't
-# change anything below unless absolutely necessary
-###
-
-# Number of processes in system run queue averaged over last minute beyond which
-# Open ONI will return a 'Server Too Busy' response; If unsure, leave at default
-# Requires core.middleware.TooBusyMiddleware in MIDDLEWARE_CLASSES
-TOO_BUSY_LOAD_AVERAGE = 64
-
-# Suggested order: https://docs.djangoproject.com/en/1.10/ref/middleware/#middleware-ordering
-MIDDLEWARE = (
-    'django.middleware.security.SecurityMiddleware',
-    'core.middleware.DisableClientSideCachingMiddleware',             # Open ONI
-    'core.middleware.TooBusyMiddleware',                              # Open ONI
-    'django.middleware.http.ConditionalGetMiddleware',                # Open ONI
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    # Open ONI
+    'themes.default',
+    'core',
 )
 
-CACHES = {
-    'default': {
-        'BACKEND': 'django.core.cache.backends.filebased.FileBasedCache',
-        'LOCATION': '/var/tmp/django_cache',
-        'TIMEOUT': 60 * 60 * 24 * 7 * 8
+"""
+'From' address on general emails sent by Django:
+If sending email from different server, replace `@' + url.netloc` with host.
+Space at the end of EMAIL_SUBJECT_PREFIX intentional
+to separate subject from prefix.
+"""
+DEFAULT_FROM_EMAIL = 'YOUR_PROJECT_NAME_ABBREVIATION-no-reply@' + url.netloc
+EMAIL_SUBJECT_PREFIX = '[YOUR_PROJECT_NAME_ABBREVIATION] '
+
+
+################################################################
+# OPEN-ONI SETTINGS
+################################################################
+"""
+SITE_TITLE that will be used for display purposes throughout app.
+PROJECT_NAME may be the same as SITE_TITLE but can be used for longer
+descriptions that will only show up occasionally.
+For example: 'Open ONI' for most headers, 'Open Online Newspapers Initiative'
+for introduction / about / further information / etc
+"""
+SITE_TITLE = 'YOUR_SHORT_PROJECT_NAME'
+PROJECT_NAME = 'YOUR_LONG_PROJECT_NAME'
+
+# Relative path from core and theme apps to subdirectory with essay templates.
+# For example: 'essays' will find files in themes/*/templates/essays
+ESSAY_TEMPLATES = 'essays'
+
+"""
+Use below only if LoC is down and MARC requests fail.
+We've mirrored a *lot* of MARC records on GitHub for use with
+"""
+#MARC_RETRIEVAL_URLFORMAT = 'https://raw.githubusercontent.com/open-oni/marc-mirror/master/marc/%s/marc.xml'
+"""
+To serve locally, clone open-oni/marc-mirror repository
+to static/compiled/marc and use setting below.
+MARC files may be updated periodically with getlc.go Go script.
+"""
+#MARC_RETRIEVAL_URLFORMAT = BASE_URL + '/static/marc/%s/marc.xml'
+
+"""
+Number of processes in system run queue averaged over last minute beyond which
+Open ONI will return a 'Server Too Busy' response. If unsure, leave at default.
+Requires core.middleware.TooBusyMiddleware in MIDDLEWARE.
+"""
+TOO_BUSY_LOAD_AVERAGE = 64
+
+
+if DEBUG:
+    """
+    DEBUG mode (not production) disables Django cache, sends emails to console,
+    and adds middleware that disables all client-side caching.
+    SQL logging is optional in DEBUG mode with env var ONI_LOG_SQL.
+    Note that SQL logging will override logging to a file with ONI_LOG_TO_FILE.
+    """
+    CACHES = {
+        'default': {
+            'BACKEND': 'django.core.cache.backends.dummy.DummyCache'
+        }
     }
-}
 
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'handlers': {
-        'console': {
-            'class': 'logging.StreamHandler',
-        },
-    },
-    'loggers': {
-        'django': {
-            'handlers': ['console'],
-            'level': os.getenv('DJANGO_LOG_LEVEL', 'INFO'),
-        },
-    },
-}
+    # Output emails to console
+    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
-# Fingerprint compiled static files with MD5 hash of contents
-# Store hashes in STATIC_ROOT directory as staticfiles.json
-STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
+    # Suggested order: https://docs.djangoproject.com/en/2.2/ref/middleware/#middleware-ordering
+    MIDDLEWARE = (
+        'django.middleware.security.SecurityMiddleware',
+        'core.middleware.DisableClientSideCachingMiddleware',         # Open ONI
+        'core.middleware.TooBusyMiddleware',                          # Open ONI
+        'django.middleware.http.ConditionalGetMiddleware',            # Open ONI
+        'django.contrib.sessions.middleware.SessionMiddleware',
+        'django.middleware.common.CommonMiddleware',
+        'django.middleware.csrf.CsrfViewMiddleware',
+        'django.contrib.messages.middleware.MessageMiddleware',
+        'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    )
 
-# Relative path from core and theme apps to subdirectory where essay templates are stored
-# example: "essays" would find files in themes/default/templates/essays
-ESSAY_TEMPLATES = "essays"
+    if os.getenv('ONI_LOG_SQL', 0) == '1':
+        LOGGING = {
+            'version': 1,
+            'disable_existing_loggers': False,
+            'filters': {
+                'require_debug_true': {
+                    '()': 'django.utils.log.RequireDebugTrue',
+                }
+            },
+            'handlers': {
+                'console': {
+                    'level': 'DEBUG',
+                    'filters': ['require_debug_true'],
+                    'class': 'logging.StreamHandler',
+                }
+            },
+            'loggers': {
+                'django.db.backends': {
+                    'level': 'DEBUG',
+                    'handlers': ['console'],
+                }
+            }
+        }
+
+
+else:
+    """
+    Production mode (DEBUG is off) uses filesystem cache, adds middleware for
+    optional error reporting emails, and fingerprints static files
+    """
+    CACHES = {
+        'default': {
+            'BACKEND': 'django.core.cache.backends.filebased.FileBasedCache',
+            'LOCATION': '/var/tmp/django_cache',
+            'TIMEOUT': 60 * 60 * 24 * 7 * 8  # Eight weeks
+        }
+    }
+
+    # Suggested order: https://docs.djangoproject.com/en/2.2/ref/middleware/#middleware-ordering
+    MIDDLEWARE = (
+        'django.middleware.security.SecurityMiddleware',
+        'core.middleware.TooBusyMiddleware',                          # Open ONI
+        'django.middleware.http.ConditionalGetMiddleware',            # Open ONI
+        'django.contrib.sessions.middleware.SessionMiddleware',
+        'django.middleware.common.CommonMiddleware',
+        'django.middleware.csrf.CsrfViewMiddleware',
+        'django.contrib.messages.middleware.MessageMiddleware',
+        'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    )
+
+    # Fingerprint compiled static files with MD5 hash of contents
+    # Store hashes in STATIC_ROOT directory as staticfiles.json
+    STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
+
