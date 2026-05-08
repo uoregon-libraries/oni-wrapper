f5847d30ab2adfb83e0cda78bb5c1238009a2fb2
Jeremy Echols <jechols@uoregon.edu>
2026-05-08 09:48:59 -0700

    Pin to map plugin tag
    With more Django changes coming, we should use tags for all plugins

--- checkout.sh
+++ checkout.sh
@@ -9,7 +9,7 @@ set -eu
 # production.  Note that an env variable can be used for development work,
 # rather than editing this file.
 openoni_checkout=${openoni_checkout:-v1.2.0}
-plugin_map_checkout=${plugin_map_checkout:-main}
+plugin_map_checkout=${plugin_map_checkout:-v0.6.0}
 plugin_staticpages_checkout=${plugin_staticpages_checkout:-v3.2.0}
 plugin_calendar_checkout=${plugin_calendar_checkout:-v0.6.0}
 plugin_title_locations_checkout=${plugin_title_locations_checkout:-v0.3.0}
